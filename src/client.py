cf0cae09f7ef314da2376532d27667c9692bec51
Alex Lopez <alexlopezmellina@gmail.com>
2018-03-29 13:15:08 +0200

    Creado un init para pasarle el host por parametro + script para que sea en un terminal+correción de errores, no funcional nos falta declarar las variables del server
    Co-authored-by: Gwen <gwmeba@gmail.com>

--- src/client.py
+++ src/client.py
@@ -3,57 +3,13 @@
 Intervals sample
 @author: Alejandro Lopez Mellina & Gwenaelle Mege Barriola
 '''
-
-
-from pyactor.context import set_context, create_host, Host, sleep, shutdown
-from pyactor.exceptions import TimeoutError
-
-
-class Server(object):
-    _ask = {'add', 'wait_a_lot'}
-    _tell = ['substract']
-
-    def add(self, x, y):
-        return x + y
-
-    def substract(self, x, y):
-        print 'subtract', x - y
-
-    def wait_a_lot(self):
-        sleep(2)
-        return 'ok'
-
-class client (object):
-  _ask = {}
-  _tell=['countWords','wordCounts']
-  
-  def countWords(self,msg):
-    #hacer conteo de palabras en el mensaje
-    #acceder al servidor http y conseguir el texto
-    print msg
-  def wordCounts(self,msg):
-    #hacer conteo de palabras en el mensaje
-    pass
+from pyactor.context import set_context, create_host, serve_forever
 
 
 if __name__ == "__main__":
     set_context()
-    host = create_host('http://127.0.0.1:1679')
+    host = create_host('http://127.0.0.1:1277/')
 
-    remote_host = host.lookup_url('http://127.0.0.1:1277/', Host)
-    print remote_host
-    server = remote_host.spawn('server', 'client/Server')
-    z = server.add(6, 7)
-    print z
-    server.substract(6, 5)
-    t = server.add(8, 7)
-    print t
-    client = remote_host.spawn('client','client/client') 
-    client.countWords('hola')
-    try:
-        print server.wait_a_lot(timeout=1)
-    except TimeoutError, e:
-        print e
+    print 'host listening at port 1277'
 
-    sleep(3)
-    shutdown()
+    serve_forever()
