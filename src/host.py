92768a16118bb3178d59e4c42e384ba61fe555d6
Alex Lopez <alexlopezmellina@gmail.com>
2018-04-07 23:50:18 +0200

    Ahora con partición de ficheros y cada mapper coge un fichero
    Co-authored-by: Gwen <gwmeba@gmail.com>

--- src/host.py
+++ src/host.py
@@ -11,7 +11,7 @@ import collections, os, datetime, timeit
 
 start = timeit.default_timer()
 class Server(object):
-    _ask = {''}
+    _ask = {'parsefile'}
     _tell = ['init_st', 'gestionCount', 'gestionWord']
     _ref = ['gestionCount', 'gestionWord', 'init_st']
 
@@ -27,34 +27,36 @@ class Server(object):
         self.reducer = host.spawn('reducer', 'host/Reducer')  
         print 'se han inicializado todos los mappers y el reducer'
 
-    def gestionCount(self, x):
+    def parsefile(self, file, nmapper, nMappers):
+        ca=open(file, 'r').readlines()
         
-        ca=open(x, 'r').read().replace('\n', ' %')
+        ca1 = open(file+str(nmapper), 'w')
 
-        li= ['*',';',',','.','-','$','!','"','%','&','/','\\','(',')',':','=','?',']','+','<','>','{',']','^']
-        for a in li:
-            ca=ca.replace(a, '')
-        ca=ca.split()
-        
+        if(nmapper==0): 
+            ca1.write(str(ca[0:(len(ca)/3)]))
+        if(nmapper==1): 
+            ca1.write(str(ca[(len(ca)/3):2*(len(ca)/3)]))
+        if(nmapper==2): 
+            ca1.write(str(ca[2*(len(ca)/3):(len(ca))]))
+        ca1.close()
+
+    def gestionCount(self, x):
+
         start = timeit.default_timer()
-        self.mapper.countWords(ca[0:(len(ca)/3)], self.reducer)
-        print 'caca'
-        self.mapper1.countWords(ca[(len(ca)/3):2*(len(ca)/3)], self.reducer)
-        print 'asd'
-        self.mapper2.countWords(ca[2*(len(ca)/3):(len(ca))], self.reducer) 
+        for i in range(0,3):
+            self.parsefile(x,i,3)
+        self.mapper.countWords(x,0, self.reducer)
+        self.mapper1.countWords(x,1, self.reducer)
+        self.mapper2.countWords(x,2, self.reducer) 
 
     def gestionWord(self, x):
-
-        ca=open(x, 'r').read()
-        
-        li= ['*',';',',','.','-','$','!','"','%','&','/','\\','(',')',':','=','?',']','+','<','>','{','[','^']
-        for a in li:
-            ca=ca.replace(a, '')
-        ca=ca.split()
         start = timeit.default_timer()
-        self.mapper.wordCount(ca[0:(len(ca)/3)], self.reducer)
-        self.mapper1.wordCount(ca[(len(ca)/3):2*(len(ca)/3)], self.reducer)
-        self.mapper2.wordCount(ca[2*(len(ca)/3):(len(ca))], self.reducer) 
+        ca=open(x, 'r').read()
+        for i in range(0,3):
+            self.parsefile(x,i,3)
+        self.mapper.wordCount(x, 0, self.reducer)
+        self.mapper1.wordCount(x, 1, self.reducer)
+        self.mapper2.wordCount(x, 2, self.reducer) 
         
 
 
@@ -63,11 +65,19 @@ class Mapper (object):
   _tell=['countWords', 'wordCount']
   _ref=['countWords', 'wordCount']
   
-  def countWords(self, lineas, reducer):
-    reducer.reduceCount(len(lineas))
+  def countWords(self, file, idmap, reducer):
+    ca=open(file+str(idmap), 'r').read()
+    li= ['*',';',',','.','-','$','!','"','%','&','/','\\','(',')',':','=','?',']','+','<','>','{','[','^']
+    for a in li:
+        ca=ca.replace(a, '')
+    reducer.reduceCount(len(ca.split()))
 
-  def wordCount(self, fi, reducer):      
-    reducer.reduceWord(collections.Counter(map(str.lower,fi)))
+  def wordCount(self, file, idmap, reducer):
+    ca=open(file+str(idmap), 'r').read()
+    li= ['*',';',',','.','-','$','!','"','%','&','/','\\','(',')',':','=','?',']','+','<','>','{','[','^','\\n']
+    for a in li:
+        ca=ca.replace(a, '')
+    reducer.reduceWord(collections.Counter(map(str.lower,ca.split(' '))))
 
 class Reducer (object):
     _ask = {''}
@@ -91,8 +101,9 @@ class Reducer (object):
         self.mappersW= self.mappersW -1
         self.count=self.count+collections.Counter(x)
         if (self.mappersW==0):
-            print timeit.default_timer() - start
+            val= timeit.default_timer() - start
             print dict(self.count)
+            print val
 
 
 if __name__ == "__main__":
